fb3f2b27be62b41542bcbdcb914bb43fcd77fcf6
Vasudeva8278 <111454632+Vasudeva8278@users.noreply.github.com>
2025-04-09 15:42:10 +0530

    commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+      username: "postgres",
+      password: "vasu1234",
+      database: "vasudev",
+      host: "localhost",
+      dialect: "postgres",
+    }
+  };
+  
